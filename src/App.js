07610285299d3a6e4ab8b83176e70a1adbe7fb55
Maralasai <115242495+Maralasai@users.noreply.github.com>
2023-09-19 21:53:05 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import GreenLightRedLight from "./components/GreenLightRedLight";
+
+function App() {
+  return (
+    <div className="App">
+      <GreenLightRedLight/> 
+    </div>
+  );
+}
+
+export default App;
